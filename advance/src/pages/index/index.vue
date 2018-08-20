96b2d25e9e0da9a4242ac368da12279d7836d685
unknown <76901l>
2018-08-20 15:17:54 +0800

    我的读书

---
+++ advance/src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  methods: {
+    getUserInfo () {
+      console.log('登录')
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+</style>
